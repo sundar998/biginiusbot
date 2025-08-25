198c59fee4db6e864411a053b7ce38fddd4ea7af
sundar998 <sundar19921@gmail.com>
2025-08-25 19:40:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,20 @@
+import os
+import re
 
+class Config:
+    # Required env vars
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "0"))
+    API_HASH = os.environ.get("API_HASH", "")
+    MONGO_URL = os.environ.get("MONGO_URL", "")
+
+    # Your Telegram database channel ID (add bot as admin there)
+    # Example: -1001234567890
+    DATABASE_CHANNEL = int(os.environ.get("DATABASE_CHANNEL", "-1001234567890"))
+
+    # Welcome image URL
+    WELCOME_PIC = os.environ.get("WELCOME_PIC", "https://envs.sh/F-V.jpg")
+
+    # Regex used to extract Unique ID from your channel message
+    # Matches things like: "🆔 Unique ID: 246810121" (case-insensitive, flexible spaces)
+    UNIQUE_ID_REGEX = re.compile(r"(?i)unique\s*id\s*[:\-]\s*([A-Za-z0-9_\-\.]+)")
